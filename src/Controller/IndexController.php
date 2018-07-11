c91baffc39fafd319f9d6c304aaafe0eae2f2093
Thomas Hubert <mr.thomas.hubert@gmail.com>
2018-07-12 00:18:17 +0200

    fix bootstrap

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,28 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wilder9
- * Date: 11/07/18
- * Time: 23:38
- */
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class indexControler extends AbstractController
+class IndexController extends AbstractController
 {
     /**
      * @Route("/")
      */
-    public function number()
+    public function index()
     {
-        $number = 45;
 
-        return $this->render('index.html.twig', array(
-            'number' => $number,
-        ));
+        return $this->render('index.html.twig');
     }
 }
 
